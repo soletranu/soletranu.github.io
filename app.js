78ad7d9122803baa9fdb1eb88800bb12efec98d9
mauriciomichels <mauriciomichels@hotmail.com>
2016-11-29 22:22:58 -0200

    initial

---
+++ app.js
@@ -0,0 +1,148 @@
+﻿var actualWord = null;
+var actualPlayer = {};
+var countSuccess = 0;
+var time = 30;
+var skips = 3;
+var tips = 1;
+
+
+speechSynthesis.getVoices();
+
+var skipWord = function(argument) {
+	skips--;
+	nextWord();
+	var word = document.querySelector('#word').value = "";
+	document.querySelector('#skips').innerHTML = skips;
+	resetTimer();
+	setTimer();
+
+	if(skips == 0){
+		document.querySelector('#skips').style.color = "#ff0000";
+		document.querySelector('#btnPular').disabled = true;
+	}
+}
+
+var setTimer = function(){
+	window.timer = setInterval(function(){
+			time -= 1;
+			document.querySelector('#time').innerHTML = time;
+			if(time <= 5)
+				document.querySelector('#time').style.color = "#ff0000";
+			if(time < 0){
+				resetTimer();
+				confirm('Seu tempo acabou!\nTotal de pontos: '+countSuccess+'\n\n');
+				window.location.reload();
+			}
+		}, 1000);
+}
+
+var getTip = function(argument) {
+
+	if(actualWord.word.length > 10)
+		alert(actualWord.word.slice(0, 3).toUpperCase()+' ... '+actualWord.word.slice(-3).toUpperCase() + '\n\n');
+	else if(actualWord.word.length > 8)
+		alert(actualWord.word.slice(0, 2).toUpperCase()+' ... '+actualWord.word.slice(-2).toUpperCase() + '\n\n');
+	else if(actualWord.word.length > 6)
+		alert(actualWord.word.slice(0, 1).toUpperCase()+' ... '+actualWord.word.slice(-1).toUpperCase() + '\n\n');
+	else 
+		alert(actualWord.word.slice(0, 1).toUpperCase()+' ... \n\n');
+
+	tips --;
+	if(tips == 0)
+		document.querySelector('#btnDica').disabled = true;
+}
+
+var resetTip = function(argument) {
+	document.querySelector('#btnDica').disabled = false;
+	tips = 1;
+}
+
+var resetTimer = function(argument) {
+	clearInterval(timer);
+	window.timer = null;
+	time = 30;
+	document.querySelector('#time').innerHTML = time;
+	document.querySelector('#time	').style.color = "#000000";	
+}
+
+var speak = function(word) {
+	var speech = new SpeechSynthesisUtterance();
+	speech.voice =  speechSynthesis.getVoices()[15];
+	// text.voiceURI = 'Google português do Brasil'; //discovered after dumping getVoices()
+  	// text.lang = "pt-BR";
+	speech.text = word;
+	speech.rate = 1;
+	speech.pitch = 1;
+	speech.volume = 1;
+
+	speechSynthesis.speak(speech);
+}
+
+var getNotUsedWord = function() {
+	var index = Math.floor(Math.random() * (words.length + 1));
+	return words[index];
+}
+
+var removeUsedWord = function(word) {
+	words.splice(word, 1);
+}
+
+var speakActualWord = function() {
+	speak(actualWord.word);
+}
+
+var speakActualDefinition = function() {
+	speak(actualWord.definition);
+}
+
+var initGame = function() {
+	var name = document.querySelector('#name').value;
+	if (name) {
+		setName(name);
+		gameMode();
+		actualWord = getNotUsedWord();
+		setTimer();
+	} else {
+		alert('Informe um nome de usuário!\n\n');
+	}
+}
+
+var setName = function(name) {
+	actualPlayer.name = name;
+	document.querySelector('#nameLabel').innerHTML = actualPlayer.name;
+}
+var verifyWord = function() {
+	var word = document.querySelector('#word').value;
+	if (word.toUpperCase() == actualWord.word.toUpperCase()) {
+		alert('Parabéns, você acertou a palavra: '+actualWord.word+'! +1 ponto\nTecle "OK" para continuar.\n\n');
+		countSuccess++;
+		nextWord();
+		document.querySelector('#points').innerHTML = countSuccess;
+		var word = document.querySelector('#word').value = "";
+		resetTimer();
+		setTimer();
+		resetTip();
+	} else {
+		confirm('Errou! \nPalavra correta era: ' + actualWord.word + '\nTotal de pontos: '+countSuccess+'\n\n');
+		window.location.reload()
+	}
+}
+
+var nextWord = function() {
+	removeUsedWord(actualWord);
+	actualWord = getNotUsedWord();
+}
+
+var initMode = function() {
+	document.querySelector('#init').style.display = 'block';
+	document.querySelector('#game').style.display = 'none';
+	countSuccess = 0;
+	document.querySelector('#points').innerHTML = countSuccess;
+}
+
+var gameMode = function() {
+	document.querySelector('#game').style.display = 'block';
+	document.querySelector('#init').style.display = 'none';
+}
+
+initMode();
---
+++ config.js
@@ -0,0 +1,277 @@
+﻿var words = [{
+	word: "Acuar",
+	definition: "Dobrar as pernas e encolher o corpo próximo do chão."
+}, {
+	word: "Aplacar",
+	definition: "Reduzir a um estado de calma."
+}, {
+	word: "Apocopar",
+	definition: "Truncar o final de uma palavra por supressão de sílaba ou letra."
+}, {
+	word: "Arfonte",
+	definition: "Que respira com dificuldade."
+}, {
+	word: "Arretar",
+	definition: "Fazer voltar para trás."
+}, {
+	word: "Burra",
+	definition: "Cofre para guardar dinheiro, joias ou documentos importantes."
+}, {
+	word: "Escurão",
+	definition: "Momento final do crepúsculo."
+}, {
+	word: "Exornar",
+	definition: "Ornamentar, enfeitar."
+}, {
+	word: "Exorar",
+	definition: "Procurar convencer, comover com súplicas."
+}, {
+	word: "Jadeita",
+	definition: "Espécie de mineral monoclínico constituído de silicato de sódio e alumínio."
+}, {
+	word: "Lênia",
+	definition: "Manha especialmente com palavras cheias de artifícios e astucias para persuadir e convencer."
+}, {
+	word: "Lhano",
+	definition: "Sincero, leal, franco, desafetado, simples."
+}, {
+	word: "Mansarda",
+	definition: "Habitação pobre."
+}, {
+	word: "Peculato",
+	definition: "Crime do desvio de verba, do furto de dinheiro ou bem móvel apreciável por parte de funcionário público, em proveito próprio ou de terceiros; abuso de confiança pública."
+}, {
+	word: "Secesso",
+	definition: "Apartamento, retiro."
+}, {
+	word: "Ucharia",
+	definition: "Armazém."
+}, {
+	word: "Admoesta",
+	definition: "perdão, isenção, arrego, repreensão, reprimenda."
+}, {
+	word: "Alarido",
+	definition: "confusão , algazarra , farra."
+}, {
+	word: "Alcoólatra",
+	definition: "indivíduo viciado em álcool"
+}, {
+	word: "Alcunha",
+	definition: "apelido"
+}, {
+	word: "Alfândega",
+	definition: "Administração pública que fiscaliza as mercadorias que entram ou saem do país"
+}, {
+	word: "Anticonstitucionalissimamente",
+	definition: "de modo muito anticonstitucional"
+}, {
+	word: "Âmago",
+	definition: "parte muito interior , cerne."
+}, {
+	word: "Aracnídeo",
+	definition: " Relativo a aracnídeo. Aranha."
+}, {
+	word: "Ardiloso",
+	definition: "manhoso , esperto."
+}, {
+	word: "Arroubo",
+	definition: "entusiasmo , fervor , encanto."
+}, {
+	word: "Auscultador",
+	definition: "Aquele que ausculta. Sonda."
+}, {
+	word: "Autoescola",
+	definition: "escola de condução"
+}, {
+	word: "Balbúrdia",
+	definition: "baderna , bagunça , confusão."
+}, {
+	word: "Beatificação",
+	definition: "[Religião] Cerimônia pela qual o Papa declara digna de veneração alguma pessoa falecida."
+}, {
+	word: "Belicoso",
+	definition: "que incita à guerra."
+}, {
+	word: "Besugo",
+	definition: "pequeno peixe acantopterígio vulgar;"
+}, {
+	word: "Bonançoso",
+	definition: "Tranquilo; sereno; propício."
+}, {
+	word: "Dilapidar",
+	definition: "desperdiçar , estragar , destruir."
+}, {
+	word: "Engodar",
+	definition: "mentir, enganar."
+}, {
+	word: "Esbaforidamente",
+	definition: "ofegante"
+}, {
+	word: "Exacerbado",
+	definition: "exagerado"
+}, {
+	word: "Exceder",
+	definition: "ir além de"
+}, {
+	word: "Excesso",
+	definition: "falta de moderação"
+}, {
+	word: "Fenecimento",
+	definition: "fim , término"
+}, {
+	word: "Fugaz",
+	definition: "passageiro , que passa rápido."
+}, {
+	word: "Fleumático",
+	definition: "imperturbável."
+}, {
+	word: "Frugal",
+	definition: "simples."
+}, {
+	word: "Frugífero",
+	definition: "frutífero"
+}, {
+	word: "Homizio",
+	definition: "refúgio, guarida, abrigo, esconderijo"
+}, {
+	word: "Ígneo",
+	definition: "próprio do fogo."
+}, {
+	word: "Ignóbil",
+	definition: "sem caráter , vergonhoso."
+}, {
+	word: "Incomensurável",
+	definition: "que não se pode medir"
+}, {
+	word: "Implícito",
+	definition: "escondido , não expresso , omisso."
+}, {
+	word: "Insolente",
+	definition: "desaforado , desagradável."
+}, {
+	word: "Irrupção",
+	definition: "entrada violenta , pancada forte."
+}, {
+	word: "Incólume",
+	definition: "intacto."
+}, {
+	word: "Inócuo",
+	definition: "inofensivo."
+}, {
+	word: "Janota",
+	definition: "bem vestida."
+}, {
+	word: "Justapor",
+	definition: "colocar perto."
+}, {
+	word: "Loquaz",
+	definition: "falador."
+}, {
+	word: "Melindre",
+	definition: "delicadeza"
+}, {
+	word: "Melissorafia",
+	definition: "descrição dos costumes das abelhas"
+}, {
+	word: "Mendicidade",
+	definition: "ato de mendigar"
+}, {
+	word: "Minucioso",
+	definition: "detalhista"
+}, {
+	word: "Nefropata",
+	definition: "quem sofre de doença dos rins"
+}, {
+	word: "Nictofobia",
+	definition: "medo doentio da noite"
+}, {
+	word: "Nódoa",
+	definition: "sujeira , mancha. pode ser também a alcunha de uma pessoa de má fama."
+}, {
+	word: "Obeso",
+	definition: "que tem obesidade"
+}, {
+	word: "Obsoleto",
+	definition: "que caiu em desuso"
+}, {
+	word: "Obumbrar",
+	definition: "cobrir-se de sombras"
+}, {
+	word: "Óbvio",
+	definition: "claro"
+}, {
+	word: "Occipital",
+	definition: "osso posterior do crânio"
+}, {
+	word: "Otorrinolaringologista",
+	definition: "Parte da Medicina que se ocupa do estudo e do tratamento das doenças dos ouvidos, do nariz e da garganta."
+}, {
+	word: "Pachorrento",
+	definition: "calmo , sereno , acomodado."
+}, {
+	word: "Pacóvio",
+	definition: "imbecil , ignorante."
+}, {
+	word: "Parco",
+	definition: "moderado , econômico , diminuto."
+}, {
+	word: "Pedante",
+	definition: "nojento , exibido , audacioso."
+}, {
+	word: "Perdulário",
+	definition: "que gasta mais."
+}, {
+	word: "Perene",
+	definition: "que dura muito , imortal."
+}, {
+	word: "Permuta",
+	definition: "troca , câmbio."
+}, {
+	word: "Pernóstico",
+	definition: "pretensioso , esnobe."
+}, {
+	word: "Petiz",
+	definition: "criança , adolescente."
+}, {
+	word: "Plissado",
+	definition: "com rugas."
+}, {
+	word: "Perscrutar",
+	definition: "vasculhar , procurar , revirar."
+}, {
+	word: "Pândego",
+	definition: "feliz , alegre."
+}, {
+	word: "Pérfido",
+	definition: "cruel , traidor , desgraçado."
+}, {
+	word: "Ruar",
+	definition: "sair sem destino , andar à toa."
+}, {
+	word: "Recôndito",
+	definition: "escondido, encoberto, secreto, oculto"
+}, {
+	word: "Rubicundo",
+	definition: "avermelhado."
+}, {
+	word: "Sumidade",
+	definition: "personalidade importante , sábio."
+}, {
+	word: "Suscitar",
+	definition: "fazer surgir , encorajar , provocar."
+}, {
+	word: "Tergiversar",
+	definition: "desculpar-se."
+}, {
+	word: "Taciturno",
+	definition: "calado."
+}, {
+	word: "Tênue",
+	definition: "fraco , frágil."
+}, {
+	word: "Veneta",
+	definition: "ataque , acesso de loucura"
+}, {
+	word: "Patronesse",
+	definition: "Mulher que é convidada para ser madrinha dos estudantes que vão se formar"
+}]
